71dd229df2b1186701118ae24fc09d2542cdcfea
Elliefffish <s110321053@mail1.ncnu.edu.tw>
2024-12-26 13:01:30 +0800

    Update for api.py

--- service/backend/update_status.py
+++ service/backend/update_status.py
@@ -11,10 +11,11 @@ def update_package_periodically(package_id = '', min=3):
     while package_id != '' :
         # Fetch the latest package_id from the Status table
         cursor.execute("SELECT status_id FROM Packages WHERE package_id = (?)", (package_id,))
-        current_status_id = cursor.fetchone()[0]
-
+        if cursor.fetchone()!= None:
+            current_status_id = cursor.fetchone()[0]
+    
         # Check if the latest_package_id is new and not processed yet
-        if current_status_id !=None and current_status_id > 1:
+        if current_status_id > 1:
             # Update the Package table
             cursor.execute("UPDATE Packages SET status_time = ?, status_id = ? WHERE package_id = ?", (datetime.datetime.now(), current_status_id-1, package_id))
             conn.commit()
@@ -28,4 +29,4 @@ def update_package_periodically(package_id = '', min=3):
     conn.close()  # Close the connection (this line will only be reached if the loop breaks)
 
 # Start the periodic update process
-update_package_periodically('PKG9691', 1)
+#update_package_periodically('PKG9691', 1)
